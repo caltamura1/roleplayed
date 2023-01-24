6a598a53029f03a328b6b91d516ff6a0699f3a4e
caltamura1 <104522142+caltamura1@users.noreply.github.com>
2023-01-24 14:17:16 -0500

    added formatting and json parsing

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import os
 import openai
+import json
 from keys import api_key
 
 
 openai.api_key = api_key
 
-start_sequence = "\nAI:"
-restart_sequence = "\nHuman: "
+# start_sequence = "\nAI:"
+# restart_sequence = "\nHuman: "
+human_input = input("Human: ")
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="Do you understand math?",
+  prompt= human_input,
   temperature=1,
   max_tokens=150,
   top_p=1,
@@ -19,4 +20,9 @@ response = openai.Completion.create(
   stop=[" Human:", " AI:"]
 )
 
-print(response)
+ai_json = response
+ai_json_parse = json.loads(json.dumps(ai_json))
+ai_text = ai_json_parse['choices'][0]['text']
+ai_text_form = ai_text.lstrip()
+
+print("AI: " + ai_text_form)
