421c65c67fbe1a5fc79cccdb33ff35b93b830a15
Christian Altamura <RemoteUser@ChrisWork.chalie.srv>
2023-01-26 16:16:00 -0500

    Polished DM setup

---
+++ dialogue.py
@@ -0,0 +1,20 @@
+# This is the text that will set the landscape for the scene. 
+# It must be written in 1st person as this is who the AI will pretend to be.
+
+dm_prep = """I am a Dungeon Master who has created a world called Abyss. 
+It is a world located completely beneath the surface. An entirely free 
+land where all can do as they please. Trading is vital for survival here. 
+Merchants must cross through dangerous tunnels full of thieves and monsters. 
+Typically they hire adventurers for protection, but hire the wrong ones 
+and it could mean certain doom. Respond to the prompts you are provided in as
+fun and imaginitive way as possible.\n\n"""
+
+npc_prep = """You will pretend to be a character named Darwin. He is an Artificer who loves crafting
+powerful inventions. You're not very strong, but you are well connected, quick witted, and have many weapons
+at your disposal."""
+
+ai_prep = dm_prep
+
+
+##### Notes Dump #####
+# Hello there sir. I am a merchant looking for an adventurer who will offer me safe passage through the tunnels. Can you help?
--- main.py
+++ main.py
@@ -1,41 +1,33 @@
 import openai
 import json
 from keys import api_key
+from dialogue import ai_prep
 
 openai.api_key = api_key
 
-all_input     = ""
-prev_input    = ""
-all_history   = ""
+all_input    = ""
+all_history, prev_input = ai_prep, ai_prep
 
-#need to find a good way to tune the AI before start.
-
-ai_prep = """I want you to pretend to be an NPC from the roleplaying game Dungeons and Dragons.
-I am about to give you a lot of information about the world space you exist in. Use it to develop your character."""
-
-# world_setting = input("""\nHello Game Master! 
-# This is a program designed to create real time NPCs so you don't have to.
-# Please enter in as much background information on your world as possible.
-# The AI will memorize and base its responses on your submission.
-# Then you may ask it to role play as a character.
-# And continue the conversation from there.\n\n
-# Please describe your world: """)
+start_sequence = "\nPlayer: "
+restart_sequence = "\n\nDM: "
 
 while True:
-  human_input = input("Human: ")
+  human_input = input("Player: ")
 
   if human_input == "stop":
     print("Exiting. Thanks for playing! Here is your chat history!", end="\n\n")
-    print(all_history)
+    # Commenting out to reduce clutter during debug
+    # print(all_history)
     break
 
   else:
-    all_input = prev_input + human_input
+    fhuman_input = human_input
+    all_input = prev_input + fhuman_input
     response = openai.Completion.create(
       model="text-davinci-003",
       prompt= all_input,
-      temperature=1,
-      max_tokens=2000,
+      temperature=.9,
+      max_tokens=1000,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=1
@@ -46,9 +38,12 @@ while True:
     ai_text = ai_json_parse['choices'][0]['text']
     ai_text_format = ai_text.lstrip()
 
-    print("\nAI: " + ai_text_format, end = "\n\n")
+    print("\nNPC: " + ai_text_format, end = "\n\n")
     prev_input = all_input + ai_text_format
 
-    human_history = ("Human: " + human_input + "\n\n")
-    ai_history    = ("AI: " + ai_text_format + "\n")
+    ### Debug Code ###
+    print("\n\n\n" + prev_input)
+
+    human_history = ("Player: " + fhuman_input + "\n\n")
+    ai_history    = ("NPC: " + ai_text_format + "\n")
     all_history   = (all_history + human_history + ai_history + "\n")
